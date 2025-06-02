ba04528fea6ccc3d872d1291d1d5444551074965
alexanderrjstj <alexanderpaolo.rojas@tajamar365.com>
2025-06-02 20:32:15 +0200

    SE CERRÓ LA SESIÓN AAAAAAAAAAAAAH

---
+++ src/api.js
@@ -0,0 +1,27 @@
+// src/api.js
+import axios from 'axios';
+
+// Cambia esta URL según la IP pública o dominio de tu servidor
+const URL_BASE = 'http://TU_IP_PUBLICA/api';
+
+const api = axios.create({
+    baseURL: URL_BASE,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+});
+
+// Interceptor para incluir el token JWT en cada request
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    error => Promise.reject(error)
+);
+
+export default api;
